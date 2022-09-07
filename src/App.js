1e2abdf9ec40107040d9cb654db202ee31a5534c
ViditaRustagi <30573120+ViditaRustagi@users.noreply.github.com>
2022-09-08 00:59:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div className="Container">
+        <div className="Box Box1">
+          <div className="BoxContent">Box1</div>
+        </div>
+        <div className="Box Box2">
+          <div className="BoxContent">Box2</div>
+        </div>
+        <div className="Box Box3">
+          <div className="BoxContent">Box3</div>
+        </div>
+      </div>
+    </div>
+  );
+}
